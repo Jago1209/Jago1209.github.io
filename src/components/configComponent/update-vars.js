567d539bd1262c81dd20bca0ae751f4ce251aca1
desarrolladorjunior9 <desarrollador9@procesamiento.com.co>
2022-03-03 10:01:40 -0500

    second commit

--- src/components/configComponent/update-vars.js
+++
@@ -1,5 +0,0 @@
-//
-export const Rol = sessionStorage.getItem('Atlantico-rol');
-export const Dependence = sessionStorage.getItem('Atlantico-dependence');
-export const logo_map = './assets/icons/logos/logo-small.png';
-export const title_sidebar = 'Modelo Extendido';
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap';
-
-import { getFormulario } from './components/pageComponent/formulario/formulario';
-
-export var token = '';
-export var params = new URLSearchParams(location.search);
-export var id_form = params.get('id_form');
-
-getFormulario(id_form,token,params);
